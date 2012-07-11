e394d751f4cd64821211825885aea8f903e4f78e
@tiredpixel <tiredpixel@gmail.com>
2012-07-11 20:00:50 +0100

    Change storage setting for uploaders to be file if no FOG_HOST.
    If no FOG_HOST is configured, the uploaders will use file-storage.
    This enables the application to be run without access to a CDN.

--- app/uploaders/lilypond_uploader.rb
+++ app/uploaders/lilypond_uploader.rb
@@ -12,7 +12,7 @@ class LilypondUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 #  storage :file
-  storage :fog
+  storage (ENV['FOG_PROVIDER'].blank? ? :file : :fog)
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
