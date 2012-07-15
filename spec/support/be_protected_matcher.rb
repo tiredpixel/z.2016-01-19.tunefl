7ab7a5e2b9e2097b2db949b0e9feb6e4951a3f7f
@tiredpixel <tiredpixel@gmail.com>
2012-07-15 16:13:12 +0100

    Add custom RSpec be_accessible, be_protected matchers.

---
+++ spec/support/be_protected_matcher.rb
@@ -0,0 +1,11 @@
+RSpec::Matchers.define :be_protected do |attribute|
+  match do |response|
+    response.class.attribute_names.include?(attribute.to_s) && !response.class.accessible_attributes.include?(attribute)
+  end
+  
+  description { "be protected :#{attribute}" }
+  
+  failure_message_for_should { ":#{attribute} should be protected" }
+  
+  failure_message_for_should_not { ":#{attribute} should not be protected" }
+end
