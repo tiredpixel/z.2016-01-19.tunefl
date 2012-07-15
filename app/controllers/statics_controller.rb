5516e2060cd82f77cb715efa93e0d33b7bc85cae
@tiredpixel <tiredpixel@gmail.com>
2012-07-15 15:24:33 +0100

    Document StaticsController actions.

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,12 +1,16 @@
 class StaticsController < ApplicationController
+  
+  # GET /
   def index
     expires_in 15.minutes, :public => true
   end
   
+  # GET /legal
   def legal
     expires_in 1.hour, :public => true
   end
   
+  # GET /sitemap.xml
   def sitemap
     expires_in 1.hour, :public => true
   end
