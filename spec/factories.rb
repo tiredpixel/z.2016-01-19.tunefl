b4bb674dade62c9a316f93b60d20c159c188c354
tiredpixel <tp@tiredpixel.com>
2013-03-21 21:02:37 +0000

    Flatten factories into a single file.
    This makes the factories, which will likely each only be a few lines,
    more easily maintainable.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
+  
   factory :admin_user do
     sequence(:email) { |n| "au#{n}-#{Time.now.to_i}@example.com" }
     password "password"
   end
+  
+  factory :score do
+    music "\relative c' { c d8 e f g a b c1 }"
+  end
+  
 end
