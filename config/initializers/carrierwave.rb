654861346ab346724b95f3bb8b97f9a2f49b52b3
tiredpixel <tp@tiredpixel.com>
2013-06-03 23:30:51 +0100

    Revert "Change ENV vars to lowercase, unless magical."
    This reverts commit 1fe89dbfb358dc340d79e829475c695b1b8b73ce.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,12 @@
 require 'carrierwave'
 require 'json'
 
-
-if ENV['fog_credentials']
+if ENV['FOG_CREDENTIALS']
   CarrierWave.configure do |config|
-    config.fog_credentials = JSON.parse(ENV['fog_credentials']).with_indifferent_access
+    config.fog_credentials = JSON.parse(ENV['FOG_CREDENTIALS']).with_indifferent_access
     
-    config.fog_directory = ENV['fog_directory']
+    config.fog_directory = ENV['FOG_DIRECTORY']
     
-    config.asset_host = ENV['asset_host']
+    config.asset_host = ENV['ASSET_HOST']
   end
 end
